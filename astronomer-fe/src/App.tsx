1c2757b2a5c123b55a7a4d9d1c705e8a8e8e39cd
Chris James <xcastroj97@gmail.com>
2024-08-09 21:45:58 +0800

    familiarise with react router

--- astronomer-fe/src/App.tsx
+++ astronomer-fe/src/App.tsx
@@ -1,10 +1,22 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+
 import Header from "./components/Header/Header";
+import Employee from "./pages/Employee/Employee";
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
-    </>
+      <Routes>
+        <Route path="/" element={<Employee />} />
+        <Route path="/employee" element={<Navigate to="/" />} />
+      </Routes>
+    </Router>
   );
 }
 

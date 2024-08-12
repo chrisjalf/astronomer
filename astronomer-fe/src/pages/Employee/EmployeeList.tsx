9b0dee0fd42ebc9fc90dbbc3773608b6b2c95cc3
Chris James <xcastroj97@gmail.com>
2024-08-12 13:24:39 +0800

    kickstart employee list

--- astronomer-fe/src/pages/Employee/EmployeeList.tsx
+++ astronomer-fe/src/pages/Employee/EmployeeList.tsx
@@ -1,3 +1,64 @@
 export default function EmployeeList() {
-  return <>Employee List</>;
+  return (
+    <div className="container my-5">
+      <div className="table-responsive">
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Department</th>
+              <th>Status</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Alibaba</td>
+              <td>IT</td>
+              <td>Active</td>
+              <td>
+                <div
+                  className="btn-group"
+                  role="group"
+                  aria-label="Basic mixed styles example"
+                >
+                  <button type="button" className="btn btn-primary">
+                    View
+                  </button>
+                  <button type="button" className="btn btn-warning">
+                    Edit
+                  </button>
+                  <button type="button" className="btn btn-danger">
+                    Delete
+                  </button>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td>Boost</td>
+              <td>Finance</td>
+              <td>Inactive</td>
+              <td>
+                <div
+                  className="btn-group"
+                  role="group"
+                  aria-label="Basic mixed styles example"
+                >
+                  <button type="button" className="btn btn-primary">
+                    View
+                  </button>
+                  <button type="button" className="btn btn-warning">
+                    Edit
+                  </button>
+                  <button type="button" className="btn btn-danger">
+                    Delete
+                  </button>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }

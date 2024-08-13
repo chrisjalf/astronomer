b48d9b870690dd926cb873160d3f661078f15bfd
Chris James <xcastroj97@gmail.com>
2024-08-13 23:12:48 +0800

    return dummy data for the get employees endpoint

--- astronomer-be/src/employee/employee.controller.ts
+++ astronomer-be/src/employee/employee.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { EmployeeService } from './employee.service';
 
 @Controller('employee')
 export class EmployeeController {
   constructor(private readonly employeeService: EmployeeService) {}
+
+  @Get('all')
+  async allEmployees() {
+    return this.employeeService.findAll();
+  }
 }
--- astronomer-be/src/employee/employee.service.ts
+++ astronomer-be/src/employee/employee.service.ts
@@ -1,4 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
 
 @Injectable()
-export class EmployeeService {}
+export class EmployeeService {
+  async findAll() {
+    return [
+      {
+        id: new Date().toString(),
+        name: "Alibaba",
+        department: "IT",
+        status: "Active",
+        number: 1123398609,
+        email: "chris.w4ac@gmail.com",
+        address1: "24-5 Residensi Vivo",
+      },
+    ];
+  }
+}
